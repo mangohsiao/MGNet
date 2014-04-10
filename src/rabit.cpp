72439c6c858a282a4c369fbb3f442604ca6a841f
mangohsiao <mango.d.hsiao@gmail.com>
2014-04-11 02:26:21 +0800

    add rabit.cpp

---
+++ src/rabit.cpp
@@ -0,0 +1,24 @@
+/*
+ * rabit.cpp
+ *
+ *  Created on: Apr 10, 2014
+ *      Author: xiao
+ */
+
+//keep alive.
+/*	rtvl = setsockopt(sock_fd, SOL_SOCKET, SO_KEEPALIVE, (char*)&keepAlive, sizeof(keepAlive));
+	if(rtvl < 0){
+		printf("setsockopt SO_KEEPALIVE failed.");
+	}
+	rtvl = setsockopt(sock_fd, SOL_TCP, TCP_KEEPIDLE, (char*)&timezero, sizeof(timezero));
+	if(rtvl < 0){
+		printf("setsockopt TCP_KEEPIDLE failed.");
+	}
+	rtvl = setsockopt(sock_fd, SOL_TCP, TCP_KEEPINTVL, (char*)&timeout, sizeof(timeout));
+	if(rtvl < 0){
+		printf("setsockopt TCP_KEEPINTVL failed.");
+	}
+	rtvl = setsockopt(sock_fd, SOL_TCP, TCP_KEEPCNT, (char*)&times_snd, sizeof(times_snd));
+	if(rtvl < 0){
+		printf("setsockopt TCP_KEEPCNT failed.");
+	}*/
